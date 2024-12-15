1220f9e6e47ea5036d7a1c0e93bf9b981d894907
Keith Parum <23403050+keithparum@users.noreply.github.com>
2024-12-15 11:18:46 +0100

    trying to figure out how to make a grid in js

---
+++ main.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+const gridBuilder = (numRows, numColumns) {
+
+}
+
+for (i = 0; i < ; i++) {
+  container.innerHTML = "<div class='cell'></div>";
+}
