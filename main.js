15bb73a2a6c86c5baffc0aee9c87858aeb2e4cc5
Keith Parum <23403050+keithparum@users.noreply.github.com>
2024-12-15 14:48:44 +0100

    added function and logic to create cells and color background on hover

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 "use strict";
 
 const container = document.querySelector(".container");
+const button = document.querySelector("#resize-button");
 
 function gridBuilder(rows, columns) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       const gridItem = document.createElement("div");
       gridItem.className = "grid-item";
+      gridItem.addEventListener("mouseover", () => {
+        const randomColor = `rgb(${Math.floor(Math.random() * 256)}, 
+                                  ${Math.floor(Math.random() * 256)}, 
+                                  ${Math.floor(Math.random() * 256)})`;
+        gridItem.style.backgroundColor = randomColor;
+      });
       container.appendChild(gridItem);
     }
   }
